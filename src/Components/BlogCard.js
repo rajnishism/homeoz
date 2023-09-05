b48b39fa695c072dc061c4be50b6f3ea8992b85c
rajnishism <starajnish8@gmail.com>
2023-09-06 01:53:20 +0530

    error : resolve blog card bg cover

--- src/Components/BlogCard.js
+++ src/Components/BlogCard.js
@@ -4,7 +4,7 @@ const BlogCard = () => {
   return (
     <div>
       <div className="    h-80 w-64 m-8 flex dropshadow-md hover:drop-shadow-3xl  rounded  bg-primary bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc91ct5acUkGR_GweFRa9MHaY4yMAy29x8VXWpEuNB-w&s')] bg-cover bg-center ">
-        <div className="sbg flex items-end h-80 w-64 rounded bg-[url('file:///Users/rajnish/Downloads/output-onlinepngtools.png')] bg-cover bg-center">
+        <div className="sbg flex items-end h-80 w-64 rounded  bg-cover bg-center">
           <div className="text-white pb-4 px-4 font-bold">
             Addressing Allergies with Homeopathy: Finding Relief Naturally
           </div>
